b4eb879ace7c188f1bbb6d9cd50b0e6566a839e6
aaronlin986 <46300650+aaronlin986@users.noreply.github.com>
2021-06-30 14:55:51 -0400

    started login form

---
+++ src/components/LoginForm.js
@@ -0,0 +1,42 @@
+import {useState} from 'react'
+import '../componentStyles/LoginForm.css'
+
+const LoginForm = ({show}) => {
+    const [username, setUsername] = useState('')
+    const [password, setPassword] = useState('')
+
+    const submitLogin = (e) => {
+        e.preventDefault()
+    }
+
+    return (
+        <div class='modal' style={{display: {show}}}>
+            <div class='modal-content'>
+                <div class='close'>
+                    &times;
+                </div>
+                <form onSubmit={submitLogin}>
+                    <div> 
+                        Username: <input 
+                                    type='text'
+                                    value={username} 
+                                    onChange={({target}) => setUsername(target.value)}
+                                    placeholder='Enter a username'
+                                />
+                    </div>
+                    <div>
+                        Password: <input 
+                                    type='password'
+                                    value={password} 
+                                    onChange={({target}) => setPassword(target.value)}
+                                    placeholder='Enter a password'
+                                />
+                    </div>
+                    <button type='submit'>Login</button>
+                </form>
+            </div>
+        </div>
+    )
+}
+
+export default LoginForm
