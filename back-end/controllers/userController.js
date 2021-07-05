b286f615a0f15715f09b858cf0c4185709dd5760
aaronlin986 <46300650+aaronlin986@users.noreply.github.com>
2021-07-05 12:39:46 -0400

    Implemented backend for login and included bcrypt

--- back-end/controllers/userController.js
+++ back-end/controllers/userController.js
@@ -1,12 +1,14 @@
 const sequelize = require('sequelize');
 const models = require('../models');
+const bcrypt = require('bcrypt')
 
 exports.createUser = async (username, password) => {
     let newUser = {};
+    let passwordHash = await bcrypt.hash(password, 10);
     try {
         newUser = await models.User.create({
             Username: username,
-            Password: password
+            PasswordHash: passwordHash
         });
     } catch(error) {
         if (error instanceof sequelize.UniqueConstraintError) {
@@ -18,4 +20,16 @@ exports.createUser = async (username, password) => {
         };
     }
     return { ok: 'Success', user: newUser};
+}
+
+exports.login = async (username, password) => {
+    let user = {};
+    user = await models.User.findOne({where: {username}});
+    let accExists = user === null 
+        ? false
+        : bcrypt.compare(password, user.PasswordHash);
+    if(!accExists){
+        return {error: 'Username or password is incorrect', reason: error};
+    }
+    return user;
 }
--- back-end/models/user.js
+++ back-end/models/user.js
@@ -9,7 +9,7 @@ module.exports = (sequelize, DataTypes) => {
             type: DataTypes.STRING,
             unique: true
         },
-        Password: {
+        PasswordHash: {
             type: DataTypes.CHAR(60),
             allowNull: false
         },
